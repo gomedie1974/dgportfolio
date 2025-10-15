8feebba14efbe544894ace3844399b917bb5215a
Diego Gomez <gomedie1974@hotmail.com>
2025-10-15 15:39:37 -0300

    boton subir

--- script.js
+++ script.js
@@ -61,3 +61,19 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+
+/* Para boton arriba */
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  });
+
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
